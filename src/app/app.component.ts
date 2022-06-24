2693d8d251aa9a3c31a56f32b6d90984afa387fd
kvr836 <107458466+kvr836@users.noreply.github.com>
2022-06-24 13:32:46 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hospital Seva';
+  myimage:string="https://thumbs.dreamstime.com/b/doctors-nurses-8151070.jpg";
+}
